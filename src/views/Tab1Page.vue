bd119fd548445bc0f0265e5ac570a37c575cf15f
Enzo <enzoluiz.brt@gmail.com>
2022-12-15 15:22:35 -0300

    geolocation

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,29 +1,48 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-fab>
+        <ion-fab-button @click="printCurrentPosition()" >
+          <ion-icon :icon="map"></ion-icon
+        ></ion-fab-button>
+      </ion-fab>
+      <br>
+      <br>
+      <br>
+      <br>  
+      <div v-html="Latitude"></div>
+      <div v-html="Longitude"></div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { Geolocation } from "@capacitor/geolocation";
+import { defineComponent, ref } from "vue";
+import { map } from "ionicons/icons";
+import { IonPage, IonContent } from "@ionic/vue";
 
-export default  defineComponent({
-  name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+export default defineComponent({
+  name: "Tab1Page",
+  components: {
+    IonContent,
+    IonPage,
+  },
+  setup() {
+    let Latitude = ref("<b>Latitude:</b> ");
+    let Longitude = ref("<b>Longitude:</b> ");
+    const printCurrentPosition = async () => {
+      const coordinates = await Geolocation.getCurrentPosition();
+      Latitude.value += coordinates.coords.latitude;
+      Longitude.value += coordinates.coords.longitude;
+      console.log("Current position:", coordinates);
+    };
+    return {
+      printCurrentPosition,
+      Latitude,
+      Longitude,
+      map,
+    };
+  },
 });
 </script>
